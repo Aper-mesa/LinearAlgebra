5c24ba8ee3e65284060d3496f05e89d94dc96c34
邹上豪 <zoush@kean.edu>
2023-11-07 19:50:54 +0800

    更新README

--- LinearAlgebra/src/LinearEquation.java
+++ LinearAlgebra/src/LinearEquation.java
@@ -6,6 +6,7 @@ import java.util.Scanner;
 public class LinearEquation extends Console {
     private static final Scanner input = new Scanner(System.in);
     private static int numberOfEquations = 0;
+
     private LinearEquation() {
     }
 
